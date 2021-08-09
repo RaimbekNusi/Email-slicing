f0a2d62f10e3aa219491bf58f0c0fc99c9695dc1
RaimbekNusi <86201781+RaimbekNusi@users.noreply.github.com>
2021-08-09 23:57:47 +0600

    Add files via upload

---
+++ a3q1.py
@@ -0,0 +1,9 @@
+"Raimbek Nussipkhozhin, NFU892, 11313819, Jeffrey Long"
+email = input("Enter your email address: ")
+at = email.find("@")
+username = email[0:at]
+left = email[at+1:len(email)]
+dot = left.find(".")
+domain = left[0:dot]
+print("The username is: " + username)
+print("The domain is: " + domain)
